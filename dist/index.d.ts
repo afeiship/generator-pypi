47d50fc8f1f59eb5fc811c21681d1eb6977c3c8a
afeiship <1290657123@qq.com>
2020-12-23 17:44:46 +0800

    feat: use gulp as build tools

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,2 +1,10 @@
+/*!
+ * name: @boilerplate-scope/boilerplate-typescript-package
+ * description: boilerplate-typescript-package-description
+ * homepage:
+ * version: 1.0.0
+ * date: 2020-12-23 17:44:26
+ * license: MIT
+ */
 declare const _default: () => void;
 export default _default;
--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,12 @@
 "use strict";
+/*!
+ * name: @boilerplate-scope/boilerplate-typescript-package
+ * description: boilerplate-typescript-package-description
+ * homepage:
+ * version: 1.0.0
+ * date: 2020-12-23 17:44:26
+ * license: MIT
+ */
 exports.__esModule = true;
 exports["default"] = (function () {
     console.log('hello');
