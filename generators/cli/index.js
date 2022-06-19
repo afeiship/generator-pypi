44c9a5608f05e55f0ae76cca901efe5c42c75570
afeiship <1290657123@qq.com>
2022-06-19 22:23:43 +0800

    feat: add cli generator

---
+++ generators/cli/index.js
@@ -0,0 +1,34 @@
+"use strict";
+const Generator = require("yeoman-generator");
+const yosay = require("yosay");
+const globby = require("globby");
+const yoHelper = require("@jswork/yeoman-generator-helper");
+const genp = require("@jswork/generator-prompts");
+const prompts = genp(["author", "email", "project_name", "description"]);
+
+module.exports = class extends Generator {
+  async prompting() {
+    this.log(
+      yosay(`Welcome to the stunning $ "generator-cli" generator!`)
+    );
+
+    this.props = await this.prompt(prompts);
+  }
+
+  writing() {
+    const { project_name } = this.props;
+    const ctx = yoHelper.ctx;
+    const opts = { ...this.props, ctx };
+    this.fs.copyTpl(
+      globby.sync(this.templatePath("**"), { dot: true }),
+      this.destinationPath(),
+      opts,
+      null,
+      {
+        processDestinationPath: (filePath) => {
+          return filePath.replace("jsw_nx", ctx.underscored(project_name));
+        },
+      }
+    );
+  }
+};
---
+++ generators/cli/templates/README.md
@@ -0,0 +1,16 @@
+# <%= project_name %>
+> <%= description %>
+
+## installation
+```shell
+pip install <%= project_name %> -U
+```
+
+## usage
+```shell
+$ <%= project_name %> --help
+Usage: <%= project_name %> [OPTIONS]
+
+Options:
+  --help              Show this message and exit.
+```
